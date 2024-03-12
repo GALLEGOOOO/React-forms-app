c0f4042ef8ed80804c6b6b5cc2bb4d7758225777
GALLEGO <48623129+GALLEGOOOO@users.noreply.github.com>
2024-03-12 16:42:11 +0100

    yt

---
+++ src/Filter.jsx
@@ -0,0 +1,11 @@
+
+
+const Filter = ({ filterValue, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filterValue} onChange={handleFilterChange} />
+    </div>
+  );
+};
+
+export default Filter;
